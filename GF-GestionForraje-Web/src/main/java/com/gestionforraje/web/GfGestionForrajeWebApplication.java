21a73e2f4f7cbb6f6e4d86f313d5e06ee1d02e7e
m4rc3l0sg <marcelo.amaro@estudiantes.utec.edu.uy>
2022-05-10 12:07:24 -0300

    Commit inicial con Eclipse. Close #2

---
+++ GF-GestionForraje-Web/src/main/java/com/gestionforraje/web/GfGestionForrajeWebApplication.java
@@ -0,0 +1,13 @@
+package com.gestionforraje.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfGestionForrajeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfGestionForrajeWebApplication.class, args);
+	}
+
+}
