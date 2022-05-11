f0c1379106e8aa0f1f6f10429bc2a04149bdd9f8
m4rc3l0sg <marcelo.amaro@estudiantes.utec.edu.uy>
2022-05-11 10:03:30 -0300

    Usuario y Perfil Entities creadas. Close #3

---
+++ GF-GestionForraje-Web/src/main/java/com/gestionforraje/web/entity/Usuario.java
@@ -0,0 +1,215 @@
+package com.gestionforraje.web.entity;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Transient;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Usuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8391331119696809355L;
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long id;
+	
+	@Column
+	private String nombre;
+	
+	@Column
+	private String apellido;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String usuario;
+	
+	@Column
+	private String password;
+	
+	@Transient
+	private String confirmarpassword;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "usuario_perfiles",
+				joinColumns = @JoinColumn(name="usuario_id"),
+				inverseJoinColumns = @JoinColumn(name="perfil_id"))
+	private Set<Perfil> perfiles;
+	
+	public Usuario() {
+		super();
+		
+	}
+
+	public Usuario(Long id) {
+		super();
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmarpassword() {
+		return confirmarpassword;
+	}
+
+	public void setConfirmarpassword(String confirmarpassword) {
+		this.confirmarpassword = confirmarpassword;
+	}
+
+	public Set<Perfil> getPerfiles() {
+		return perfiles;
+	}
+
+	public void setPerfiles(Set<Perfil> perfiles) {
+		this.perfiles = perfiles;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + ", usuario="
+				+ usuario + ", password=" + password + ", confirmarpassword=" + confirmarpassword + ", perfiles="
+				+ perfiles + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((confirmarpassword == null) ? 0 : confirmarpassword.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((perfiles == null) ? 0 : perfiles.hashCode());
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (confirmarpassword == null) {
+			if (other.confirmarpassword != null)
+				return false;
+		} else if (!confirmarpassword.equals(other.confirmarpassword))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (perfiles == null) {
+			if (other.perfiles != null)
+				return false;
+		} else if (!perfiles.equals(other.perfiles))
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
