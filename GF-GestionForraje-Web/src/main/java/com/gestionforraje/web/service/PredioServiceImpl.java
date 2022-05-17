c1f4f13407188bf92c1d414350128a23c2938320
m4rc3l0sg <marcelo.amaro@estudiantes.utec.edu.uy>
2022-05-17 13:09:04 -0300

    Servicio para "Crear Predios" y validacion por Nombre, Finalizados. Close # 16

--- GF-GestionForraje-Web/src/main/java/com/gestionforraje/web/service/PredioServiceImpl.java
+++ GF-GestionForraje-Web/src/main/java/com/gestionforraje/web/service/PredioServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gestionforraje.web.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,22 @@ public class PredioServiceImpl implements PredioService{
 	@Override
 	public Iterable<Predio> getAllPredios() {
 		return repository.findAll();
+	}
+
+	private boolean checkNombreAvailable(Predio predio) throws Exception{
+		Optional<Predio> predioFound = repository.findByNombre(predio.getNombre());
+		if(predioFound.isPresent()){
+			throw new Exception("El nombre de predio ya existe");
+		}
+		return true;
+	}
+
+	@Override
+	public Predio createPredio(Predio predio) throws Exception {
+		if(checkNombreAvailable(predio)) {
+			predio = repository.save(predio);
+		}
+		return predio;
 	}
 
 }
